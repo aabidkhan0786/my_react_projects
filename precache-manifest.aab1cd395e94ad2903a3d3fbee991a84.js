ddf11fc04c3f5751ea8cae5c709d6e559ec68d79
aabidkhan0786 <62328527+aabidkhan0786@users.noreply.github.com>
2020-08-16 15:35:31 +0530

    Updates

---
+++ precache-manifest.aab1cd395e94ad2903a3d3fbee991a84.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a94a192723f0189a15a8e82295d8fa7",
+    "url": "/my_react_projects/index.html"
+  },
+  {
+    "revision": "e6235a86fc2225248548",
+    "url": "/my_react_projects/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8b0edd0c50319cc5b20e",
+    "url": "/my_react_projects/static/css/main.d428b938.chunk.css"
+  },
+  {
+    "revision": "e6235a86fc2225248548",
+    "url": "/my_react_projects/static/js/2.6a521fa9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/my_react_projects/static/js/2.6a521fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0edd0c50319cc5b20e",
+    "url": "/my_react_projects/static/js/main.707320b1.chunk.js"
+  },
+  {
+    "revision": "4e818b422ed9d88e364c",
+    "url": "/my_react_projects/static/js/runtime-main.16dbb9db.js"
+  },
+  {
+    "revision": "baceeccadd50a3e7daa3074d5943ed12",
+    "url": "/my_react_projects/static/media/about.baceecca.gif"
+  },
+  {
+    "revision": "16524867d52bf684c864d2b78b47bc15",
+    "url": "/my_react_projects/static/media/ana.16524867.jpg"
+  },
+  {
+    "revision": "349b0c13a1745c5fe8fdb7057854c1a9",
+    "url": "/my_react_projects/static/media/d.349b0c13.jpg"
+  },
+  {
+    "revision": "dead2903c912ebecda99842c66fd3d09",
+    "url": "/my_react_projects/static/media/im1.dead2903.gif"
+  },
+  {
+    "revision": "a12ccfefe8cf0e7cdc8847e51025b5f3",
+    "url": "/my_react_projects/static/media/iot.a12ccfef.jpg"
+  },
+  {
+    "revision": "2beb83f64e8e4950a2ebb0cff47fac14",
+    "url": "/my_react_projects/static/media/social.2beb83f6.jpg"
+  },
+  {
+    "revision": "eca99c79e0ee148e30a75d913919e258",
+    "url": "/my_react_projects/static/media/soft.eca99c79.jpg"
+  },
+  {
+    "revision": "515f781d9030e3c108053e917548cb9d",
+    "url": "/my_react_projects/static/media/web.515f781d.jpg"
+  }
+]);
